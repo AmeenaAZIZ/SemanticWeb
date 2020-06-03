d8b9633061933e07e996ffbe61635ce71bd45334
walidmh <walid_mohsin10@hotmail.com>
2020-06-03 15:01:58 +0200

    Test our query
    Signed-off-by: walidmh <walid_mohsin10@hotmail.com>

--- src/components/test1.js
+++ src/components/test1.js
@@ -2,21 +2,11 @@ import React from 'react';
 import { sparqlConnect, setQueryURL } from 'sparql-connect'
 
 //Set the sparql endpoint (should be done once for the whole application)
-//setQueryURL('https://query.wikidata.org/sparql')
-setQueryURL('http://dbpedia.org/sparql');
+setQueryURL('https://query.wikidata.org/sparql')
+//setQueryURL('http://dbpedia.org/sparql');
 
 //Write a query that returns some resources with the additional field `label`
-/* SELECT DISTINCT ?women ?womenLabel
-WHERE
-{
-    ?women wdt:P31 wd:Q5 .
-    ?women wdt:P21 wd:Q6581072 .
-    ?women wdt:P106/wdt:P279* wd:Q483501 . # artists
-    SERVICE wikibase:label {bd:serviceParam wikibase:language "fr,en" }
-}
-LIMIT 500 */
-const query = `
-PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+/* PREFIX foaf: <http://xmlns.com/foaf/0.1/>
 PREFIX dbo: <http://dbpedia.org/ontology/>
 PREFIX : <http://dbpedia.org/resource/>
 SELECT DISTINCT ?name ?person
@@ -24,7 +14,21 @@ WHERE {
     ?person dbo:birthPlace :Brussels .     
     ?person foaf:name ?name .
 }
-LIMIT 10
+LIMIT 10 */
+const query = `
+PREFIX wdt:<http://www.wikidata.org/prop/direct/>
+PREFIX wd:<http://www.wikidata.org/entity/>
+PREFIX wikibase: <http://wikiba.se/ontology#>
+PREFIX bd: <http://www.bigdata.com/rdf#>
+SELECT DISTINCT ?women ?womenLabel
+WHERE
+{
+    ?women wdt:P31 wd:Q5 .
+    ?women wdt:P21 wd:Q6581072 .
+    ?women wdt:P106/wdt:P279* wd:Q483501 . # artists
+    SERVICE wikibase:label {bd:serviceParam wikibase:language "fr,en" }
+}
+LIMIT 20
 `
 //Create a connector to populate the component with the results
 const connector = sparqlConnect(query, {
@@ -37,8 +41,8 @@ function ResourcesList({ results }) {
 return (
     <ul>
     {
-        results.map(({ name, person }) => 
-        <li key={person}>{ name }</li>
+        results.map(({ women, womenLabel }) => 
+        <li key={women.value}>{ women }: {womenLabel}</li>
         )
     }
     </ul>
