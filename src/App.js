9b8119d0e4ee6360539688214bf5bc3a8f2df84d
walidmh <walid_mohsin10@hotmail.com>
2020-06-03 11:26:26 +0200

    Init app in React + Query Exemple with SparqlConnect
    Signed-off-by: walidmh <walid_mohsin10@hotmail.com>

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ResourcesList from './components/test1';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ResourcesList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/test1.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { sparqlConnect, setQueryURL } from 'sparql-connect'
+
+//Set the sparql endpoint (should be done once for the whole application)
+//setQueryURL('https://query.wikidata.org/sparql')
+setQueryURL('http://dbpedia.org/sparql');
+
+//Write a query that returns some resources with the additional field `label`
+/* SELECT DISTINCT ?women ?womenLabel
+WHERE
+{
+    ?women wdt:P31 wd:Q5 .
+    ?women wdt:P21 wd:Q6581072 .
+    ?women wdt:P106/wdt:P279* wd:Q483501 . # artists
+    SERVICE wikibase:label {bd:serviceParam wikibase:language "fr,en" }
+}
+LIMIT 500 */
+const query = `
+PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+PREFIX dbo: <http://dbpedia.org/ontology/>
+PREFIX : <http://dbpedia.org/resource/>
+SELECT DISTINCT ?name ?person
+WHERE {      
+    ?person dbo:birthPlace :Brussels .     
+    ?person foaf:name ?name .
+}
+LIMIT 10
+`
+//Create a connector to populate the component with the results
+const connector = sparqlConnect(query, {
+    queryName: 'results',
+    })
+
+//Write a component assuming the query results will be available as a prop
+//named `results`
+function ResourcesList({ results }) {
+return (
+    <ul>
+    {
+        results.map(({ name, person }) => 
+        <li key={person}>{ name }</li>
+        )
+    }
+    </ul>
+)
+}
+
+//Connect the query to your component
+export default connector(ResourcesList, {
+    loading: () => <span>Resources are loading</span>,
+    error: () => <span>Something went wrong</span>
+    })
